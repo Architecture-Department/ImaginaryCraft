ffdfa53e711fac8710095ca56c0cc83987a398d9
王小尽 <1809315974@qq.com>
2026-05-25 22:24:04 +0800

    添加更多内置动画并添加更多动画功能

--- module/ImaginaryCraft/src/main/kotlin/architecture/imaginarycraft/common/item/CannedEnkephalinItem.kt
+++ module/ImaginaryCraft/src/main/kotlin/architecture/imaginarycraft/common/item/CannedEnkephalinItem.kt
@@ -4,5 +4,4 @@ import architecture.goldenboughs_lib.api.AllOpe
 import net.minecraft.world.item.Item
 
 @AllOpe
-class CannedEnkephalinItem(properties: Properties) : Item(properties) {
-}
+class CannedEnkephalinItem(properties: Properties) : Item(properties)
